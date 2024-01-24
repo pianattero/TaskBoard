3a821013ddfb057f24c103dee77fe08b9a1dd16c
pianattero <mariapianattero@gmail.com>
2024-01-24 12:30:08 +0100

    Task board home page done

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,27 @@
 <template>
   <div>
-    <h1>My Task Board</h1>
-    <p>Tasks to keep organised</p>
-  </div>
-  <div>
-    
+    <div class="home-title">
+      <h1><img src="@/assets/icons/Logo.svg"/> My Task Board</h1>
+      <p>Tasks to keep organised</p>
+    </div>
+    <div>
+      <TaskCard/>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
+// IMPORTS
+import TaskCard from '../components/TaskCard.vue'
 </script>
+
+<style scoped lang="scss">
+.home-title {
+  margin: 2rem 0;
+
+  h1{
+    font-weight: 400;
+    font-size: 2.5rem;
+  }
+}
+</style>
